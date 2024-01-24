03dc2446edafdc57844529db3f2db099a493c3c7
Jacopo Di Matteo <jacopo.michele.dimatteo@gmail.com>
2024-01-24 15:07:27 +0100

    Implement nicer logging

--- experimentation/revolve2/experimentation/logging.py
+++ experimentation/revolve2/experimentation/logging.py
@@ -1,6 +1,53 @@
 """Functions to work with logging in a standardized way."""
 
 import logging
+from dataclasses import dataclass
+
+
+@dataclass
+class Clr:
+    """Color palette for printing in the terminal."""
+
+    # === Normal === #
+    # Shades
+    wh: str = "\033[97m"
+    gr: str = "\033[90m"
+    bk: str = "\033[30m"
+
+    # RGB
+    r: str = "\033[91m"
+    g: str = "\033[92m"
+    b: str = "\033[94m"
+
+    # CYMK
+    c: str = "\033[96m"
+    m: str = "\033[95m"
+    y: str = "\033[93m"
+
+    # === Bold === #
+    # Shades
+    WH: str = "\033[1;97m"
+    GR: str = "\033[1;90m"
+    BK: str = "\033[1;30m"
+
+    # RGB
+    R: str = "\033[1;91m"
+    G: str = "\033[1;92m"
+    B: str = "\033[1;94m"
+
+    # CYMK
+    C: str = "\033[1;96m"
+    M: str = "\033[1;95m"
+    Y: str = "\033[1;93m"
+
+    # === Styles === #
+    EM = "\033[1m"  # Bold
+    NEM = "\033[2m"  # Dim
+    IT = "\033[3m"  # Italic
+    UN = "\033[4m"  # Underline
+
+    # === Functional === #
+    E: str = "\033[0m"  # End
 
 
 def setup_logging(level: int = logging.INFO, file_name: str | None = None) -> None:
@@ -17,14 +64,29 @@ def setup_logging(level: int = logging.INFO, file_name: str | None = None) -> No
     # Furthermore, we specify the format in which we want the messages to be printed.
     logging.basicConfig(
         level=level,
-        format="[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s",
+        format=f" [%(asctime)s] [%(levelname)s] {Clr.NEM}[%(module)s]{Clr.E} %(message)s",
+        datefmt="%H:%M:%S",
     )
+    # Add color to logging levels
+    logging.addLevelName(logging.DEBUG, f"{Clr.GR}DEBUG{Clr.E}")
+    logging.addLevelName(logging.INFO, f"{Clr.G}INFO{Clr.E}")
+    logging.addLevelName(logging.WARNING, f"{Clr.Y}WARN{Clr.E}")
+    logging.addLevelName(logging.ERROR, f"{Clr.R}ERROR{Clr.E}")
+
     if file_name is not None:
         logging.root.handlers.append(logging.FileHandler(file_name))
-    logging.info("=======================================")
-    logging.info("=======================================")
-    logging.info("=======================================")
-    logging.info("New log starts here.")
+
+    log_header(logging)
+    logging.info(f"\t {Clr.G}New Log Starts Here{Clr.E}")
+    log_header(logging)
+
+
+def log_header(logging: logging.Logger) -> None:
+    """
+    Log a header
+
+    :param logging: The logger to use.
+    """
     logging.info("=======================================")
     logging.info("=======================================")
     logging.info("=======================================")
