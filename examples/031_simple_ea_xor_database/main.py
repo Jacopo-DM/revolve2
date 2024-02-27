d458eea1f2f57c8912e7d8b85f9439d8e5e80e13
Jacopo-DM <jacopo.michele.dimatteo@gmail.com>
2024-02-27 14:33:16 +0100

    Refactor: Automatic Formatting

--- examples/031_simple_ea_xor_database/main.py
+++ examples/031_simple_ea_xor_database/main.py
@@ -63,7 +63,10 @@ def select_survivors(
     :returns: A newly created population.
     """
     # TODO clean up lambda functions for clarity and optimization
-    original_survivors, offspring_survivors = population_management.steady_state(
+    (
+        original_survivors,
+        offspring_survivors,
+    ) = population_management.steady_state(
         [i.genotype for i in original_population.individuals],
         [i.fitness for i in original_population.individuals],
         [i.genotype for i in offspring_population.individuals],
@@ -180,7 +183,9 @@ def run_experiment(dbengine: Engine) -> None:
         offspring_population = Population(
             individuals=[
                 Individual(genotype=genotype, fitness=fitness)
-                for genotype, fitness in zip(offspring_genotypes, offspring_fitnesses)
+                for genotype, fitness in zip(
+                    offspring_genotypes, offspring_fitnesses
+                )
             ]
         )
 
