d50c76328e522a278698e96247c3df6607f50fb7
Jacopo-DM <jacopo.michele.dimatteo@gmail.com>
2024-03-02 16:49:55 +0100

    Update: Increase Repetitions In Config

--- examples/041_robot_brain_cmaes_database/config.py
+++ examples/041_robot_brain_cmaes_database/config.py
@@ -3,7 +3,7 @@
 from revolve2.ci_group.modular_robots_v2 import gecko_plus_v2
 
 DATABASE_FILE = "database.sqlite"
-NUM_REPETITIONS = 1
+NUM_REPETITIONS = 5
 NUM_SIMULATORS = 16
 INITIAL_STD = 0.5
 NUM_GENERATIONS = 10
