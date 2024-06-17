1e2d5cff0a4bcb7245b96cc0c8fd9f0f335bd7dc
Jacopo-DM <jacopo.michele.dimatteo@gmail.com>
2024-06-17 23:15:53 +0200

    Update: merge with upstream (3)

--- ci_group/revolve2/ci_group/genotypes/cppnwin/modular_robot/v2/_body_develop.py
+++ ci_group/revolve2/ci_group/genotypes/cppnwin/modular_robot/v2/_body_develop.py
@@ -9,6 +9,7 @@ from pyrr import Quaternion, Vector3
 from revolve2.modular_robot.body import AttachmentPoint, Module
 from revolve2.modular_robot.body.v2 import ActiveHingeV2, BodyV2, BrickV2
 
+# Determine the maximum parts available for a robots body.
 MAX_PARTS = 20
 GRID_SIZE = MAX_PARTS * 2 + 1
 
@@ -33,10 +34,12 @@ def develop(
     :param genotype: The genotype to create the body from.
     :returns: The created body.
     """
-
+    # Instantiate the CPPN network for body construction.
     body_net = multineat.NeuralNetwork()
+    # Build the CPPN from the genotype of the robot.
     genotype.BuildPhenotype(body_net)
 
+    # Here we have a queue that is used to build our robot.
     to_explore: Queue[__Module] = Queue()
     grid = np.zeros(
         shape=(GRID_SIZE, GRID_SIZE, GRID_SIZE),
@@ -46,6 +49,7 @@ def develop(
     body = BodyV2()
 
     v2_core = body.core_v2
+
     core_position = Vector3(
         [MAX_PARTS + 1, MAX_PARTS + 1, MAX_PARTS + 1], dtype=np.int_
     )
@@ -92,14 +96,14 @@ def __evaluate_cppn(
     body_net: multineat.NeuralNetwork,
     position: Vector3[np.int_],
     chain_length: int,
-) -> tuple[Any, int]:
+) -> tuple[Any, float]:
     """
     Get module type and orientation from a multineat CPPN network.
 
     :param body_net: The CPPN network.
     :param position: Position of the module.
     :param chain_length: Tree distance of the module from the core.
-    :returns: (module type, rotation_index)
+    :returns: (module type, angle)
     """
     x, y, z = position
 
@@ -119,27 +123,38 @@ def __evaluate_cppn(
     outputs = body_net.Output()
 
     # ========= Set up ========= #
-    types = {0: None, 1: BrickV2, 2: ActiveHingeV2}
-    idxs_1 = list(types.keys())
-    idxs_2 = [3, 4]
+    """We select the module type for the current position using the first output
+    of the CPPN network."""
+    # TODO
+    types = [None, BrickV2, ActiveHingeV2]
+    target_idx = max(0, int(outputs[0] * len(types) - 1e-6))
+    # types = {0: None, 1: BrickV2, 2: ActiveHingeV2}
+    # idxs_1 = list(types.keys())
+    # idxs_2 = [3, 4]
 
     # ========= get module type from output probabilities ========= #
-    type_probs = softmax(np.array(outputs)[idxs_1])
+    module_type = types[target_idx]
+    # type_probs = softmax(np.array(outputs)[idxs_1])
     # WARN Selection method is biased towards the first element
     # [ ] Figure out best selection method
     #   rng = np.random.default_rng()
     #   idx = rng.choice(idxs_1, p=type_probs)
     #   idx = np.argmax(type_probs)
-    idx = np.argmin(type_probs)
-    module_type = types[idx]
+    # idx = np.argmin(type_probs)
+    # module_type = types[idx]
 
     # ========= get rotation from output probabilities ========= #
-    rotation_probs = softmax(np.array(outputs)[idxs_2])
+    """
+    Here we get the rotation of the module from the second output of the CPPN network.
+    The output ranges between [0,1] and we have 4 rotations available (0, 90, 180, 270).
+    """
+    angle = max(0, int(outputs[0] * 4 - 1e-6)) * (np.pi / 2.0)
+    # rotation_probs = softmax(np.array(outputs)[idxs_2])
     # [ ] Figure out best selection method
     #   rotation_index = rng.choice(idxs_2, p=rotation_probs)
     #   rotation_index = np.argmax(rotation_probs)
-    rotation_index = np.argmin(rotation_probs)
-    return module_type, rotation_index
+    # angle = np.argmin(rotation_probs) * (np.pi / 2.0)
+    return module_type, angle
 
 
 def __add_child(
@@ -150,37 +165,41 @@ def __add_child(
 ) -> __Module | None:
     attachment_index, attachment_point = attachment_point_tuple
 
+    """Here we adjust the forward facing direction, and the position for the new
+    potential module."""
     forward = __rotate(module.forward, module.up, attachment_point.orientation)
     position = __vec3_int(module.position + forward)
     chain_length = module.chain_length + 1
 
+    """If grid cell is occupied, we don't make a child.
+    else, set cell as occupied
+    ERROR this is MAJOR bug!
+      The core has 4 faces with 8 attachment points each,
+          however, all 8 share share the same 'position + forward' !
+      This means that the core will always attach to the first position
+          when the cppn returns 'not None' -> aka 'top' """
+    if grid[tuple(position)] > 0:
+        # No module will be placed.
+        return None
+
+    """Now we adjust the position for the potential new module to fit the
+    attachment point of the parent, additionally we query the CPPN for child
+    type and angle of the child."""
     new_pos = np.array(
         np.round(position + attachment_point.offset), dtype=np.int64
     )
+    child_type, angle = __evaluate_cppn(body_net, new_pos, chain_length)
 
-    child_type, child_rotation = __evaluate_cppn(
-        body_net, new_pos, chain_length
-    )
-
-    # if grid cell is occupied, don't make a child
-    # else, set cell as occupied
-    # ERROR this is MAJOR bug!
-    #   The core has 4 faces with 8 attachment points each,
-    #       however, all 8 share share the same "position + forward" !
-    #   This means that the core will always attach to the first position
-    #       when the cppn returns "not None" -> aka "top"
-    if grid[tuple(position)] > 0:
+    """Here we check whether the CPPN evaluated to place a module and if the
+    module can be set on the parent."""
+    can_set = module.module_reference.can_set_child(attachment_index)
+    if (child_type is None) or (not can_set):
+        # No module will be placed.
         return None
 
-    if child_type is None:
-        return None
-
-    angle = child_rotation * (np.pi / 2.0)
+    """Now we know we want a child on the parent and we instantiate it, add the
+    position to the grid and adjust the up direction for the new module."""
     child = child_type(angle)
-
-    if not module.module_reference.can_set_child(child, attachment_index):
-        return None
-
     up = __rotate(module.up, forward, Quaternion.from_eulers([angle, 0, 0]))
     module.module_reference.set_child(child, attachment_index)
     grid[tuple(position)] += 1
@@ -196,7 +215,7 @@ def __add_child(
 
 def __rotate(a: Vector3, b: Vector3, rotation: Quaternion) -> Vector3:
     """
-    Rotates vector a a given angle around b.
+    Rotates vector a, a given angle around b.
 
     :param a: Vector a.
     :param b: Vector b.
