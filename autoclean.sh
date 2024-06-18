d6839407be24556c81e62afd7d0c0b2d78e10c57
Jacopo-DM <jacopo.michele.dimatteo@gmail.com>
2024-06-18 16:43:33 +0200

    Add: requirement texts for all examples

--- autoclean.sh
+++
@@ -1,5 +0,0 @@
-ruff clean
-black . --line-length 80
-ruff format . --preview
-ruff check . --fix --unsafe-fixes
-sourcery review . --fix --config /Users/jmdm/Documents/Projects/Thesis/code/r2j/.sourcery.yaml
---
+++ examples/autoreqd.sh
@@ -0,0 +1,19 @@
+# This script will:
+# 0. Instal pipreqs if not already installed.
+# 1. Run the following command >> pipreqs . --mode compat --force; in the
+#    current directory.
+# 2. Recursively enter all the directories and subdirectories and run the above
+#    command in each of them.
+
+# Install pipreqs if not already installed
+pip install pipreqs
+
+# Recursively enter all the directories and subdirectories
+for dir in $(find . -type d)
+do
+    # Run the command in each of the directories
+    pipreqs $dir --mode compat --force
+done
+
+# Run the command in the current directory (leave current dir as the last)
+pipreqs . --mode no-pin --force
