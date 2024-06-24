d3edc7cfb23785769a6c171e94e9d58c941557ec
Jacopo-DM <jacopo.michele.dimatteo@gmail.com>
2024-06-24 20:24:24 +0200

    BugFix: more robust fix all script

--- codetools/ruff/fix.sh
+++ codetools/ruff/fix.sh
@@ -2,11 +2,11 @@
 
 cd "$(dirname "$0")"
 
-# packages=$(../read_project_parts.sh)
+packages=$(../read_project_parts.sh)
 
 cd ../..
 
-packages=$(git ls-files | grep -E '\.py$' | xargs dirname | sort | uniq)
+# packages=$(git ls-files | grep -E '\.py$' | xargs dirname | sort | uniq)
 
 if [ -n "$packages" ]; then
     echo "$packages" | xargs
