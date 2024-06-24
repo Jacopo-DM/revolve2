c8a70244ae621f503c89e7baf17e97b2701688e1
Jacopo-DM <jacopo.michele.dimatteo@gmail.com>
2024-06-24 20:47:48 +0200

    BugFix: more robust fix all docs script

--- fixdocs.sh
+++ fixdocs.sh
@@ -6,6 +6,6 @@ cd "$(dirname "$0")"
 
 while read -r package; do
     echo "$package:"
-    docconvert $package -o rest --in-place
-    pyment $package -w
+    # docconvert $package -o rest --in-place
+    pyment $package -w -o numpydoc -d
 done < <(./codetools/read_project_parts.sh)
