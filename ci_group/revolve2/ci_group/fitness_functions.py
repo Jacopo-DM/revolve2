9dbe39188794cfb56c79d086cbf2de24f5bff952
Jacopo <jacopo.michele.dimatteo@gmail.com>
2024-07-11 18:38:21 +0200

    Update:  bookmarks

--- ci_group/revolve2/ci_group/fitness_functions.py
+++ ci_group/revolve2/ci_group/fitness_functions.py
@@ -1,51 +1,72 @@
 """Standard fitness functions for modular robots."""
 
-from itertools import pairwise, starmap
-
 import numpy as np
 from pyrr import Vector3
 from revolve2.modular_robot_simulation import ModularRobotSimulationState
 
+BIAS = Vector3((1, 1, 0))
+ABS = Vector3((0, 0, 0))
+MEASURE = "absolute"  # "absolute" or "relative"
 
-def l1_dist(a: Vector3, b: Vector3) -> float:
+
+def _l1_dist_(a: Vector3, b: Vector3, bias: Vector3 = BIAS) -> float:
     """Calculate the L1 distance between two points."""
-    return np.sum(np.abs(a - b))
+    return np.sum(np.abs((a * bias) - (b * bias)))
 
 
-def l2_dist(a: Vector3, b: Vector3) -> float:
+def _l2_dist_(a: Vector3, b: Vector3, bias: Vector3 = BIAS) -> float:
     """Calculate the L2 distance between two points."""
-    return np.sqrt(np.sum((a - b) ** 2))
+    return np.sqrt(np.sum(((a * bias) - (b * bias)) ** 2))
 
 
-def l2_squared_dist(a: Vector3, b: Vector3) -> float:
+def _l2_squared_dist_(a: Vector3, b: Vector3, bias: Vector3 = BIAS) -> float:
     """Calculate the squared L2 distance between two points."""
-    return np.sum((a - b) ** 2)
+    return np.sum(((a * bias) - (b * bias)) ** 2)
 
 
-def l1_area(states: list[ModularRobotSimulationState]) -> float:
+def l1_area(
+    states: list[ModularRobotSimulationState], *, absolute: bool = True
+) -> float:
     """Calculate the L1 area traveled on the xy-plane by a single modular."""
-    return np.sum([l1_dist(states[0], state_i) for state_i in states])
+    reference = ABS if absolute else states[0]
+    return np.sum([_l1_dist_(reference, state_i) for state_i in states])
 
 
-def l2_area(states: list[ModularRobotSimulationState]) -> float:
+def l2_area(
+    states: list[ModularRobotSimulationState], *, absolute: bool = True
+) -> float:
     """Calculate the L1 area traveled on the xy-plane by a single modular."""
-    return np.sum([l2_dist(states[0], state_i) for state_i in states])
+    reference = ABS if absolute else states[0]
+    return np.sum([_l2_dist_(reference, state_i) for state_i in states])
 
 
-def l2_squared_area(states: list[ModularRobotSimulationState]) -> float:
+def l2_squared_area(
+    states: list[ModularRobotSimulationState], *, absolute: bool = True
+) -> float:
     """Calculate the L1 area traveled on the xy-plane by a single modular."""
-    return np.sum([l2_dist(states[0], state_i) for state_i in states])
+    reference = ABS if absolute else states[0]
+    return np.sum([_l2_squared_dist_(reference, state_i) for state_i in states])
 
 
-def l1_displacement(states: list[ModularRobotSimulationState]) -> float:
+def l1_displacement(
+    states: list[ModularRobotSimulationState], *, absolute: bool = True
+) -> float:
     """Calculate the L1 distance traveled on the xy-plane by a single modular."""
-    return l1_dist(states[0], states[-1])
+    reference = ABS if absolute else states[0]
+    return _l1_dist_(reference, states[-1])
+
+
+def l2_displacement(
+    states: list[ModularRobotSimulationState], *, absolute: bool = True
+) -> float:
+    """Calculate the L1 distance traveled on the xy-plane by a single modular."""
+    reference = ABS if absolute else states[0]
+    return _l2_dist_(reference, states[-1])
 
 
 DISTANCE_FUNCTIONS = {
-    "l1": l1_dist,
-    "l2": l2_dist,
-    "l2_squared": l2_squared_dist,
+    "l1_displacement": l1_displacement,
+    "l2_displacement": l2_displacement,
     "l1_area": l1_area,
     "l2_area": l2_area,
     "l2_squared_area": l2_squared_area,
@@ -54,7 +75,7 @@ DISTANCE_FUNCTIONS = {
 
 def xy_displacement(
     states: list[ModularRobotSimulationState],
-    mode: str = "l1_area",
+    mode: str = "l2_squared_area",
 ) -> float:
     """Calculate the distance traveled on the xy-plane by a single modular."""
     if mode not in DISTANCE_FUNCTIONS:
