adb0277aef12c6e83c5b9ec81a2e540ec4972baa
Jacopo <jacopo.michele.dimatteo@gmail.com>
2024-07-11 18:41:17 +0200

    Update: split variations of 4c (2)

---
+++ examples/4_exp_setup/4c2_robot_brain_cmaes_from_ea/config.py
@@ -0,0 +1,9 @@
+"""Configuration parameters for this example."""
+
+from revolve2.ci_group.modular_robots_v2 import runner_v2
+
+INITIAL_STD = 0.5
+BODY = runner_v2()
+
+NUM_SIMULATORS = 16
+NUM_GENERATIONS = 1
