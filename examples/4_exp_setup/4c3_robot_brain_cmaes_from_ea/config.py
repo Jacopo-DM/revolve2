5f3c830a4d650ff5d6bb50def8d42b162cfc39bf
Jacopo <jacopo.michele.dimatteo@gmail.com>
2024-07-11 18:42:00 +0200

    Remove: unneeded folder

--- examples/4_exp_setup/4c3_robot_brain_cmaes_from_ea/config.py
+++
@@ -1,9 +0,0 @@
-"""Configuration parameters for this example."""
-
-from revolve2.ci_group.modular_robots_v2 import runner_v2
-
-NUM_SIMULATORS = 8
-INITIAL_STD = 0.5
-NUM_GENERATIONS = 50
-
-BODY = runner_v2()
