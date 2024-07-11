15eb567cd3fcecf1222670f9f85a7458b1873f9f
Jacopo <jacopo.michele.dimatteo@gmail.com>
2024-07-11 18:42:26 +0200

    Update: split variations of 4c (4)

---
+++ examples/4_exp_setup/4c3_robot_brain_cmaes_full_cpg/rerun.py
@@ -0,0 +1,63 @@
+"""Rerun a robot with given body and parameters."""
+
+import pickle
+from pathlib import Path
+
+from evaluator import Evaluator
+from individual import Individual
+from revolve2.experimentation.logging import setup_logging
+from revolve2.modular_robot.brain.cpg import (
+    active_hinges_to_cpg_network_structure_neighbor,
+)
+
+
+def get_genotype() -> Individual:
+    """Get the best robot from the database."""
+    with Path("best_robot.pkl").open("rb") as f:
+        return pickle.load(f)
+
+
+def get_brain():
+    """Get the best brain from the CMA-ES optimization."""
+    with Path("best_robot_brain.pkl").open("rb") as f:
+        return pickle.load(f)
+
+
+def main() -> None:
+    """Perform the rerun.
+
+
+    :rtype: None
+
+    """
+    setup_logging()
+
+    # Find all active hinges in the body
+    robot = get_genotype().genotype.develop()
+    body = robot.body
+    active_hinges = body.find_modules_of_any_type()
+
+    params = get_brain()
+
+    # Create a structure for the CPG network from these hinges.
+    # This also returns a mapping between active hinges and the index of there corresponding cpg in the network.
+    (
+        cpg_network_structure,
+        output_mapping,
+    ) = active_hinges_to_cpg_network_structure_neighbor(active_hinges)
+
+    # Create the evaluator.
+    evaluator = Evaluator(
+        headless=False,
+        num_simulators=1,
+        cpg_network_structure=cpg_network_structure,
+        body=body,
+        output_mapping=output_mapping,
+    )
+
+    # Show the robot.
+    evaluator.evaluate([params])
+
+
+if __name__ == "__main__":
+    main()
